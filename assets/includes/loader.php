26f977ca803a2d161a4a879d907f8b2716cc5ab0
Toheed Ahmad <toheedswati786@gmail.com>
2025-09-30 10:57:08 -0400

    # new updated

--- assets/includes/loader.php
+++ assets/includes/loader.php
@@ -1,6 +1,5 @@
         <div class="loader-wrap">
             <div class="preloader">
-                <div class="preloader-close">x</div>
                 <div id="handle-preloader" class="handle-preloader">
                     <div class="animation-preloader">
                         <div class="spinner"></div>
